d4077cea49fcb07ceb45f49df9a06faa9f19b3d1
Yertis <yertis.ydyrys@narxoz.kz>
2026-03-03 23:26:48 +0500

    battle:добавил интерфейс Combatant и модель результата столкновения

---
+++ src/com/narxoz/rpg/enemy/Orc.java
@@ -0,0 +1,7 @@
+package com.narxoz.rpg.enemy;
+
+public class Orc extends BasicEnemy {
+    public Orc() {
+        super("Orc", 16, 110);
+    }
+}
---
+++ src/com/narxoz/rpg/enemy/Wraith.java
@@ -0,0 +1,7 @@
+package com.narxoz.rpg.enemy;
+
+public class Wraith extends BasicEnemy {
+    public Wraith() {
+        super("Wraith", 14, 90);
+    }
+}
